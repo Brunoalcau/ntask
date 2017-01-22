62a58aa61da117707b4a5cac2b636e1581f9424b
emilianocarvalho <emilianocarvalho.it@gmail.com>
2017-01-22 15:53:23 -0300

    Adicionando controle de logs e clusters

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Documentação - Node Task API",
+  "template": {
+    "forceLanguage": "pt_br"
+  },
+  "version": "1.0.0",
+  "description": "API de serviços do Procon/PB",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-01-22T18:41:45.895Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.3"
+  }
+});
--- routes/token.js
+++ routes/token.js
@@ -1,31 +1,47 @@
 import jwt from "jwt-simple";
 
 module.exports = app => {
-  const cfg = app.libs.config;
-  const Users = app.db.models.Users;
-
-  app.post("/token", (req, res) => {
-    if (req.body.email && req.body.password) {
-      const email = req.body.email;
-      const password = req.body.password;
-      // console.log("Log TOKEN before findOne: email:" + email + " password: " + password);
-      Users.findOne({where: {email: email}})
-        .then(user => {
-          // console.log("Log TOKEN after findOne: email:" + email + " user.password: " + user.password);
-          if (Users.isPassword(user.password, password)) {
-            // console.log("Log TOKEN isPassword: email:" + email + " user.password: " + user.password);
-            const payload = {id: user.id};
-            res.json({
-              token: jwt.encode(payload, cfg.jwtSecret)
-            });
-          } else {
-            // console.log("Log TOKEN else isPassword: email:" + email + " user.password: " + user.password);
+    const cfg = app.libs.config;
+    const Users = app.db.models.Users;
+    /**
+    * @api {post} /token Token autenticado
+    * @apiGroup Credencial
+    * @apiParam {String} email Email de usuário
+    * @apiParam {String} password Senha de usuário
+    * @apiParamExample {json} Entrada
+    *  {
+    *   "email": "john@connor.net",
+    *   "password": "123456"
+    *  }
+    * @apiSuccess {String} token Token de usuário autenticado
+    * @apiSuccessExample {json} Sucesso
+    *   HTTP/1.1 200 OK
+    *   {"token": "xyz.abc.123.hgf"}
+    * @apiErrorExample {json} Erro de autenticação
+    *   HTTP/1.1 401 Unauthorized
+    */
+    app.post("/token", (req, res) => {
+        if (req.body.email && req.body.password) {
+            const email = req.body.email;
+            const password = req.body.password;
+            // console.log("Log TOKEN before findOne: email:" + email + " password: " + password);
+            Users.findOne({where: {email: email}})
+            .then(user => {
+                // console.log("Log TOKEN after findOne: email:" + email + " user.password: " + user.password);
+                if (Users.isPassword(user.password, password)) {
+                    // console.log("Log TOKEN isPassword: email:" + email + " user.password: " + user.password);
+                    const payload = {id: user.id};
+                    res.json({
+                        token: jwt.encode(payload, cfg.jwtSecret)
+                    });
+                } else {
+                    // console.log("Log TOKEN else isPassword: email:" + email + " user.password: " + user.password);
+                    res.sendStatus(401);
+                }
+            })
+            .catch(error => res.sendStatus(401));
+        } else {
             res.sendStatus(401);
-          }
-        })
-        .catch(error => res.sendStatus(401));
-    } else {
-      res.sendStatus(401);
-    }
-  });
+        }
+    });
 };
